9db41cebb288b3990db09edcf5babc6401e5c3b8
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-09 15:33:49 +0800

    Update Announcement.js

--- models/Announcement.js
+++ models/Announcement.js
@@ -4,14 +4,8 @@ const mongoose = require("mongoose");
 const announcementSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    body: { type: String, required: true },
-    audience: {
-      type: String,
-      enum: ["All", "Students", "Teachers", "Admins", "SpecificSection"],
-      default: "All",
-    },
-    section: { type: mongoose.Schema.Types.ObjectId, ref: "Section" }, // if SpecificSection
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
