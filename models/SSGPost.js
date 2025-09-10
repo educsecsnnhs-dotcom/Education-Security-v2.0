c46fe89dc669dc3e5015357c3b0716c2b9c5e31c
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-10 18:13:51 +0800

    Update SSGPost.js

--- models/SSGPost.js
+++ models/SSGPost.js
@@ -1,10 +1,10 @@
+// models/SSGPost.js
 const mongoose = require("mongoose");
 
 const ssgPostSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  authorName: { type: String, required: true }, // capture officer’s name
   createdAt: { type: Date, default: Date.now },
 });
 
