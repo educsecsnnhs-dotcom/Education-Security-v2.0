30fcfd8213c41fd4a90411105c02e0c1f8fe04e0
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-10 18:16:00 +0800

    Create ssgBoard.js

---
+++ routes/ssgBoard.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { authRequired } = require("../middleware/authMiddleware");
+const { createPost, getPosts, deletePost } = require("../controllers/ssgBoardController");
+
+// SSG Management Board
+router.post("/create", authRequired, createPost);
+router.get("/", authRequired, getPosts);
+router.delete("/:id", authRequired, deletePost);
+
+module.exports = router;
