8647b1dee652b9ee7b23607f1b7c730b0c9a89b7
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-10 18:15:41 +0800

    Update ssg.js

--- routes/ssg.js
+++ routes/ssg.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createElection, vote, viewResults } = require("../controllers/ssgController");
+const { createElection, castVote, getResults, closeElection } = require("../controllers/ssgController");
 const { authRequired } = require("../middleware/authMiddleware");
 
 // Elections (SSG)
 router.post("/create", authRequired, createElection);
-router.post("/vote", authRequired, vote);
-router.get("/results", authRequired, viewResults);
+router.post("/vote", authRequired, castVote);
+router.get("/results", authRequired, getResults);
+router.post("/close", authRequired, closeElection);
 
 module.exports = router;
