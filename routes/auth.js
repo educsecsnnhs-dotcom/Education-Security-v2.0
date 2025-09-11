11361bf6c0e062cfa929a1469715bd6ff02f0606
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-11 20:23:30 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const { authRequired, requireRole } = require("../middleware/roleMiddleware");
+const { authRequired, requireRole } = require("../middleware/authMiddleware");
 const { encryptPassword } = require("../utils/caesar");
 
 /**
